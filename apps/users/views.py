d806cfc7a5c64e3a319c212aa1b0b9a3f8639597
“Princess <anto6519@stud.kea.dk>
2019-11-08 20:16:16 +0100

    Added html for login and register html, commented out if statement in loginview to test

--- apps/users/views.py
+++ apps/users/views.py
@@ -9,7 +9,8 @@ class RegisterView(View):
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
-        if not request.user.is_anonymous:
+        # if not request.user.is_anonymous:
             # TODO redirect somewhere good!
-            return redirect('pages:index')
+            # return redirect('pages:index')
         return render(request, 'users/login.html', {})
+
