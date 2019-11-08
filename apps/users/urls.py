04f3d58434d359f0ea758789d7e7c75e152d069a
“Princess <anto6519@stud.kea.dk>
2019-11-08 21:03:23 +0100

    setting up user authentication

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
 ]
